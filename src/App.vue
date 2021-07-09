1f19a9980295e2181184d8bca3f49aea2c7ec098
Matthew <matsgeco92@hotmail.it>
2021-07-09 12:55:47 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <div>
+        <img src="./assets/millionaire.png" alt="">
+      </div>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#nav {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 40vh;
+  background: radial-gradient(circle,rgba(7,8,139,1) 9%, rgba(22,6,112,1) 71%);
+}
+
+#nav {
+  padding: 30px;
+  img {
+    width: 15%;
+    height: 15%;
+    margin-top: 30px;
+  }
+  a {
+    font-weight: bold;
+    color: white;
+    text-decoration: none;
+    &:hover {
+      color: greenyellow;
+    }
+  }
+}
+
+.home {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-color: #11093A;
+  height: 60vh;
+  text-align: center;
+  .myContainer {
+    width: 60%;
+    margin: auto;
+    color: white;
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      list-style-type: none;
+      justify-content: space-around;
+      li:first-child {
+        flex-basis: 80%;
+        margin: 60px 0;
+      }
+      li {
+        flex-basis: 45%;
+        border: 2px solid white;
+        border-radius: 50%;
+        padding: 30px 20px;
+        margin: 30px 0;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="home">
+    <div class="myContainer">
+      <ul>
+        <li><h1>{{domande[2].domanda}}</h1></li>
+        <li v-for="risp in domande[2].risposte" v-on:click="getStatus(risp)">{{risp.name}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+      
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data: function () {
+    return {
+      domande: [
+        {
+          domanda : 'Chi è il più bello del mondo?',
+          risposte : {
+            1: {
+              name: 'Matthew',
+              status: true,
+              },
+            2: {
+              name: 'Sara',
+              status: false,
+              },
+            3: {
+              name: 'Brian',
+              status: false,
+              },
+            4: {
+              name: 'Kevin',
+              status: false,
+              },
+          }
+        },
+        {
+          domanda : 'Chi è il più forte del mondo?',
+          risposte : {
+            1: {
+              name: 'Matthew',
+              status: true,
+              },
+            2: {
+              name: 'Sara',
+              status: false,
+              },
+            3: {
+              name: 'Brian',
+              status: false,
+              },
+            4: {
+              name: 'Kevin',
+              status: false,
+              },
+          }
+        },
+        {
+          domanda : 'Chi è il più effemminato del mondo?',
+          risposte : {
+            1: {
+              name: 'Matthew',
+              status: true,
+              },
+            2: {
+              name: 'Simone',
+              status: false,
+              },
+            3: {
+              name: 'Mirko',
+              status: false,
+              },
+            4: {
+              name: 'Alfonso',
+              status: false,
+              },
+          }
+        },
+        {
+          domanda : 'Chi è il più forte a Macchiavelli?',
+          risposte : {
+            1: {
+              name: 'Matthew',
+              status: true,
+              },
+            2: {
+              name: 'Matthew',
+              status: true,
+              },
+            3: {
+              name: 'Matthew',
+              status: true,
+              },
+            4: {
+              name: 'Matthew',
+              status: true,
+              },
+          }
+        },
+        {
+          domanda : 'Chi è il più grasso del mondo?',
+          risposte : {
+            1: {
+              name: 'Lori',
+              status: true,
+              },
+            2: {
+              name: 'Sara',
+              status: false,
+              },
+            3: {
+              name: 'Michela',
+              status: false,
+              },
+            4: {
+              name: 'Broni',
+              status: false,
+              },
+          }
+        },
+      ]
+    }
+  },
+  methods: {
+    getStatus: function (risp) {
+      let status = risp.status;
+      if (status) {
+        alert('GIUSTO');
+      } else {
+        alert('SBAGLIATO');
+      }
+    }
+  }
+};
+</script>
